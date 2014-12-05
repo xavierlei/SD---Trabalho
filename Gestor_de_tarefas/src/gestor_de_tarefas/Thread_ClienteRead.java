f45eadffd9544d0b145ee228a67a0aa80d94cb62
Carlos Morais <jcmorais93@gmail.com>
2014-12-05 23:45:52 +0000

    Thread ClienteRead
    vai ler o que o servidor escreve

---
+++ Gestor_de_tarefas/src/gestor_de_tarefas/Thread_ClienteRead.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestor_de_tarefas;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author carlosmorais
+ */
+
+//Classe para ler as mensagens do Servidor, no Cliente
+public class Thread_ClienteRead implements Runnable{
+    private BufferedReader inputServer;
+    
+    public Thread_ClienteRead(BufferedReader in){
+        this.inputServer = in;
+    }
+    
+    public void run(){
+        try {
+            //recebe mensagens do servidor e imprime
+            String line;
+            while( (line = inputServer.readLine()) != null ){
+                System.out.println(line);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Thread_ClienteRead.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }               
+}
