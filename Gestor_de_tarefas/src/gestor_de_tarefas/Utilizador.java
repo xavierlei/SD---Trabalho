74d2c25ab266d87ad3d2da4967df1a452ed68d6c
xavierlei <xavier.c.f@hotmail.com>
2014-12-05 11:44:24 +0000

    utilizadores

---
+++ Gestor_de_tarefas/src/gestor_de_tarefas/Utilizador.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestor_de_tarefas;
+
+/**
+ *
+ * @author xavier
+ */
+public class Utilizador {
+    
+}
