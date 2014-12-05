3763c7f9ae2496cd3a741a2fc632363dd5c2474f
Carlos Morais <jcmorais93@gmail.com>
2014-12-05 23:45:16 +0000

    Thread TrataPedido
    ficara encarregue de tratar dos pedidos do Cliente

---
+++ Gestor_de_tarefas/src/gestor_de_tarefas/Thread_TrataPedido.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestor_de_tarefas;
+
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author carlosmorais
+ */
+public class Thread_TrataPedido implements Runnable {
+    private Armazem armazem;
+    private PrintWriter out;
+    private String pedido;
+
+    public Thread_TrataPedido(Armazem armazem, PrintWriter out, String pedido) {
+        this.armazem = armazem;
+        this.out = out;
+        this.pedido = pedido;
+    }
+    
+    public void run(){
+        String resposta = null;
+        /* como é feito o pedido? String?
+            então, parser no pedido, com o switch é escolhido o metodo a executar no Armazem?
+        */
+        /*executa
+            dependendo do metodo executado, é criada a String resposta e enviada para o Cliente
+        */        
+        //envia resposta para o Clienete       
+        out.println(resposta);
+        out.flush();
+    }
+    
+    
+}
