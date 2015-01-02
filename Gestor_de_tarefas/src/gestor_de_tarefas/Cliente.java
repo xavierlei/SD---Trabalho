253973ccda1882eec5c328f8dc7c1c445aa42b5e
xavierlei <xavier.c.f@hotmail.com>
2015-01-02 17:45:43 +0000

    cenas

--- Gestor_de_tarefas/src/gestor_de_tarefas/Cliente.java
+++ Gestor_de_tarefas/src/gestor_de_tarefas/Cliente.java
@@ -33,58 +33,6 @@ public class Cliente {
             //Thread para ler as mensagens do Servidor
             Thread cr = new Thread( new Thread_ClienteRead(serverInput));
             cr.start();
-            
-            /*TESTE*/
-                      
-            output.println("login:carlos:123");
-            output.flush();
-            Thread.sleep(100);
-            output.println("abastece:martelo:10");
-            output.flush();
-            Thread.sleep(100);
-            output.println("abastece:prego:50");
-            output.flush();
-            Thread.sleep(100);
-            output.println("abastece:chave:10");
-            output.flush();
-            Thread.sleep(100);
-            output.println("adiciona:cadeira:martelo:2:prego:10");
-            output.flush();
-            Thread.sleep(100);
-            output.println("adiciona:mesa:martelo:2:chave:10");
-            output.flush();
-            Thread.sleep(100);
-            output.println("adiciona:secretaria:prego:10");
-            output.flush();
-            Thread.sleep(100);
-            output.println("adiciona:mesa de cabeçeira:prego:10");
-            output.flush();
-            Thread.sleep(100);
-            output.println("executa:cadeira");
-            output.flush();
-            Thread.sleep(100);
-            output.println("executa:mesa");
-            output.flush();
-            Thread.sleep(100);
-            output.println("executa:cadeira");
-            output.flush();
-            Thread.sleep(100);
-            output.println("executa:secretaria");
-            output.flush();
-            Thread.sleep(100);
-            output.println("executa:secretaria");
-            output.flush();
-            Thread.sleep(100);
-            output.println("executa:mesa");
-            output.flush();
-            Thread.sleep(100);
-            output.println("executa:mesa de cabeçeira");
-            output.flush();
-            Thread.sleep(100);
-            output.println("listagem");
-            output.flush();
-            
-            
             //le as mensagens do Utilizador e envia para o Servidor
             //fara sentido tratar os pedidos aqui, de forma a irem limpos para o servidor
             while( !(pedido=userInput.readLine()).equals("logout") ){
